4b3e71eb33da37754ac80b66ded9754f6e1d4a8e
Samshh <samdacs2@gmail.com>
2024-08-14 23:18:55 +0800

    vercel routing

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,15 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import App from './App';
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import App from "./App";
 
 const Router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
   {
     path: "*",
     element: <Navigate to="/" replace />,
-  }
+  },
 ]);
 
 export default Router;
